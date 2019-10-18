8430e105b0f5edeaf56e6787c9162d7149cbc083
tkoikereps <tkoike_tkoike@yahoo.co.jp>
2019-10-18 13:34:43 +0900

    タイプミス
    で、わざとテストFailにしてDeployされないかチェックする

--- svrmode.test.js
+++ svrmode.test.js
@@ -6,7 +6,7 @@ const svrmodel = require('./svrmodel');
 
 test("for Server Side Model", () => {
 	let obj = {"name":"Yamada Taro","age":"44","sex":"Man","telnum":"03-1234-5678","addrnum":"123-4567","addr":"Gunma Takasaki Shibasaki 00-00-00"};
-	expect(svrmodel.set(obj)).toBe('OK');
+	expect(svrmodel.set(obj)).toBe('NG');
 	
 	obj = {"name":"","age":"","sex":"","telnum":"","addrnum":"","addr":""};
 	expect(svrmodel.set(obj)).toBe('NG');
